57c32cb461ba09847a558fccbee03d406fe2bffc
Intruder <intruder1912@gmail.com>
2018-10-05 13:57:49 +0200

    project setup, initial commit

---
+++ src/main/java/org/doblander/itk/pos1/mavenTests/mavenTest2018/App.java
@@ -0,0 +1,13 @@
+package org.doblander.itk.pos1.mavenTests.mavenTest2018;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
